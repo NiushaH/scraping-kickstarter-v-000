f604fe6cdca5ff8dc5693a1e005d5cdbdc582a1a
Niusha H <niusha.hadziomerovic@gmail.com>
2019-04-09 15:29:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,9 +13,10 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
- 
-  projects = {}
 
+  projects = {}
+ 
+  # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
@@ -28,4 +29,8 @@ def create_project_hash
  
   # return the projects hash
   projects
+
 end
+
+
+create_project_hash
